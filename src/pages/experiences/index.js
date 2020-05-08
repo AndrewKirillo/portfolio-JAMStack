c5fcacd382befed91acddabc586ae931f36f9a94
Andrew Kirillov <kirillov.edu@gmail.com>
2020-05-08 02:05:54 -0400

    fixed exports for new index pages

--- src/pages/experiences/index.js
+++ src/pages/experiences/index.js
@@ -1,3 +1,21 @@
 import React from 'react'
 
 import Layout from '../../components/Layout'
+
+
+export default class ExperiencesIndexPage extends React.Component {
+    render() {
+      return (
+        <Layout>
+          <section className="section">
+            <div className="container">
+              <div className="content">
+                Experiences
+              </div>
+            </div>
+          </section>
+        </Layout>
+      )
+    }
+  }
+  
--- src/pages/organizations/index.js
+++ src/pages/organizations/index.js
@@ -1,3 +1,21 @@
 import React from 'react'
 
 import Layout from '../../components/Layout'
+
+
+export default class OrganizationsIndexPage extends React.Component {
+    render() {
+      return (
+        <Layout>
+          <section className="section">
+            <div className="container">
+              <div className="content">
+                Organizations
+              </div>
+            </div>
+          </section>
+        </Layout>
+      )
+    }
+  }
+  
