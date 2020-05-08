a8e560736550bd9ffd63f119e2443690c6c9984e
Andrew Kirillov <kirillov.edu@gmail.com>
2020-05-08 15:28:43 -0400

    changed config.yml to include body fields

---
+++ src/templates/organization.js
@@ -0,0 +1,21 @@
+
+import React from 'react'
+
+import Layout from '../components/Layout'
+
+
+export default class OrganizationTemplate extends React.Component {
+    render() {
+        return (
+        <Layout>
+            <section className="section">
+            <div className="container">
+                <div className="content">
+                    Organization
+                </div>
+            </div>
+            </section>
+        </Layout>
+        )
+    }
+}
---
+++ src/templates/project.js
@@ -0,0 +1,21 @@
+
+import React from 'react'
+
+import Layout from '../components/Layout'
+
+
+export default class ProjectTemplate extends React.Component {
+    render() {
+        return (
+        <Layout>
+            <section className="section">
+            <div className="container">
+                <div className="content">
+                    Project
+                </div>
+            </div>
+            </section>
+        </Layout>
+        )
+    }
+}
